8ce0db39a55029268f927d503370b4b5f72d6e03
leonardosotelo89 <leonardosotelo89@gmail.com>
2025-09-28 04:20:01 -0300

    Actualizar list200to299.js

--- list200to299.js
+++ list200to299.js
@@ -1,22 +1,63 @@
 const list3 = []  
-  /*
- list3[0] = {  
-    intro:['deltavesintro1.mp3',   
-           'deltavesintro2.mp3',  
-           'deltavesintro3.mp3',   
-           'deltavesintro4.mp3',   
-           'deltavesintro5.mp3',   
-           'deltavesintro6.mp3',   
-           'deltavesintro7.mp3',   
-           'deltavesintro8.mp3',  
-           'deltavesintro9.mp3',   
-           'deltavesintro10.mp3',   
-           'deltavesintro11.mp3',   
-           'deltavesintro12.mp3',   
-           'deltavesintro13.mp3',   
-           'deltavesintro14.mp3' ],   
-    song:'jessicashydeltaves.mp3',  
-    text:" &nbsp&nbsp&nbsp&#160&#160&#160 pais: Lituania (🇱🇹) / interprete: Jessica shy / cancion: Dėl Tavęs  &nbsp&nbsp&nbsp&#160&#160&#160"   
+  
+  list3[0] = {  
+    intro:['heyintro1.mp3',
+           'heyintro2.mp3',  
+           'heyintro3.mp3',  
+           'heyintro4.mp3',   
+           'heyintro5.mp3',   
+           'heyintro6.mp3',   
+           'heyintro7.mp3',   
+           'heyintro8.mp3',   
+           'heyintro9.mp3',  
+           'heyintro10.mp3',   
+           'heyintro11.mp3',   
+           'heyintro12.mp3',   
+           'heyintro13.mp3',   
+           'heyintro14.mp3',   
+           'heyintro15.mp3' ],   
+    song:'ozandogulumuratbozhey.mp3',  
+    text:" &nbsp&nbsp&nbsp&#160&#160&#160 pais: Turquía (🇹🇷) / interprete: Ozan Doğulu feat. Murat Boz / cancion: Hey  &nbsp&nbsp&nbsp&#160&#160&#160"   
   }   
-  */
+
+  list3[1] = {  
+    intro:['got2luvuintro1.mp3',
+           'got2luvuintro2.mp3',  
+           'got2luvuintro3.mp3',  
+           'got2luvuintro4.mp3',   
+           'got2luvuintro5.mp3',   
+           'got2luvuintro6.mp3',   
+           'got2luvuintro7.mp3',   
+           'got2luvuintro8.mp3',   
+           'got2luvuintro9.mp3',  
+           'got2luvuintro10.mp3',   
+           'got2luvuintro11.mp3',   
+           'got2luvuintro12.mp3',   
+           'got2luvuintro13.mp3',   
+           'got2luvuintro14.mp3',   
+           'got2luvuintro15.mp3' ],   
+    song:'seanpaulalexisjordangot2luvu.mp3',  
+    text:" &nbsp&nbsp&nbsp&#160&#160&#160 pais: Jamaica (🇯🇲) Estados Unidos (🇺🇸) / interprete: Sean Paul feat. Alexis Jordan / cancion: Got 2Luv U  &nbsp&nbsp&nbsp&#160&#160&#160"   
+  }   
+
+  list3[2] = {  
+    intro:['vabbeskibidiboppyintro1.mp3',
+           'vabbeskibidiboppyintro2.mp3',  
+           'vabbeskibidiboppyintro3.mp3',  
+           'vabbeskibidiboppyintro4.mp3',   
+           'vabbeskibidiboppyintro5.mp3',   
+           'vabbeskibidiboppyintro6.mp3',   
+           'vabbeskibidiboppyintro7.mp3',   
+           'vabbeskibidiboppyintro8.mp3',   
+           'vabbeskibidiboppyintro9.mp3',  
+           'vabbeskibidiboppyintro10.mp3',   
+           'vabbeskibidiboppyintro11.mp3',   
+           'vabbeskibidiboppyintro12.mp3',   
+           'vabbeskibidiboppyintro13.mp3',   
+           'vabbeskibidiboppyintro14.mp3',   
+           'vabbeskibidiboppyintro15.mp3' ],   
+    song:'dannydarkovabbeskibidiboppy.mp3',  
+    text:" &nbsp&nbsp&nbsp&#160&#160&#160 pais: Italia (🇮🇹) / interprete: Danny Darko / cancion: Vabbè Skibidi Boppy  &nbsp&nbsp&nbsp&#160&#160&#160"   
+  }   
+  
  
