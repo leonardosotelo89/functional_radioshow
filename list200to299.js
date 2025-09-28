1cf3789716f9effbff012f404806cade30329ee8
leonardosotelo89 <leonardosotelo89@gmail.com>
2025-09-28 03:11:01 -0300

    Create list200to299.js

---
+++ list200to299.js
@@ -0,0 +1,22 @@
+const list3 = []  
+  /*
+ list3[0] = {  
+    intro:['deltavesintro1.mp3',   
+           'deltavesintro2.mp3',  
+           'deltavesintro3.mp3',   
+           'deltavesintro4.mp3',   
+           'deltavesintro5.mp3',   
+           'deltavesintro6.mp3',   
+           'deltavesintro7.mp3',   
+           'deltavesintro8.mp3',  
+           'deltavesintro9.mp3',   
+           'deltavesintro10.mp3',   
+           'deltavesintro11.mp3',   
+           'deltavesintro12.mp3',   
+           'deltavesintro13.mp3',   
+           'deltavesintro14.mp3' ],   
+    song:'jessicashydeltaves.mp3',  
+    text:" &nbsp&nbsp&nbsp&#160&#160&#160 pais: Lituania (🇱🇹) / interprete: Jessica shy / cancion: Dėl Tavęs  &nbsp&nbsp&nbsp&#160&#160&#160"   
+  }   
+  */
+ 
